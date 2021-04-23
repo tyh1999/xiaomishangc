1da49f2dcfdf3efeada0355d8c5e1e5e2e5f5802
涂源晖 <1127750855@qq.com>
2021-04-23 19:29:23 +0800

    master

--- xiaomi/src/main.js
+++ xiaomi/src/main.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Router from './router'
-import axios from 'vue-axios'
+import axios from 'axios'
 import VueAxios from 'vue-axios'
 //import env from './env'
 
 //根据前段跨域方式做调整 /a/b : /api/a/b => /a/b
-axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = '/api';
 //超时
 axios.defaults.timeout = 8000;
 //接口错误拦截
-axios.interceptors.response.use()(function(response){
+axios.interceptors.response.use(function(response){
   let res = response.data;
   //成功
   if(res.status==0){
